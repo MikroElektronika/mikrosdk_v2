b894b5f5b76be51b26be71d7d70a931b6973ec3a
Ivan Ruzavin <ivan.ruzavin@mikroe.com>
2024-07-30 10:41:23 +0200

    Fixed SQL query that caused the runtime error

--- scripts/recursive_build.py
+++ scripts/recursive_build.py
@@ -523,7 +523,7 @@ def query_database(changes_dict):
             FROM SDKToDevice
             INNER JOIN Devices ON SDKToDevice.device_uid = Devices.uid
             WHERE SDKToDevice.sdk_uid = 'mikrosdk_v{sdk_version}'
-            AND Devices.sdk_support = '1';
+            AND Devices.sdk_support = '1'
             AND SDKToDevice.device_uid NOT LIKE '%PIM%'
             AND SDKToDevice.device_uid NOT LIKE '%CARD%'
             AND SDKToDevice.device_uid NOT LIKE '%SPARKFUN%'
