de66f0677ecd08689c3e3a47ea44b7b4aec45aac
strahi-linux <strahinja.jacimovic@mikroe.com>
2024-08-23 09:57:00 +0200

    Updated boards templates

--- resources/queries/boards/board_test_1/BoardToSocket.sql
+++
@@ -1,7 +0,0 @@
--- ? is expected to be a socket_uid
-INSERT INTO BoardToSocket (
-    board_uid,socket_uid
-)
-VALUES (
-    "UNI_CLICKER",?
-);
--- resources/queries/boards/board_test_1/SDKToBoard.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO SDKToBoard (
-    sdk_uid,board_uid
-)
-VALUES (
-    "mikrosdk_v${MIKROSDK_CURRENT_VERSION}","UNI_CLICKER"
-);
