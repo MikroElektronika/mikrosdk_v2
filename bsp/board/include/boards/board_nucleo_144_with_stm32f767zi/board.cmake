a02411512bfa29846122fd09c947e84f5cebd5a1
Ivan Ruzavin <ivan.ruzavin@mikroe.com>
2024-09-13 09:46:23 +0200

    Added release boards to new-feature/boards/4

---
+++ bsp/board/include/boards/board_nucleo_144_with_stm32f767zi/board.cmake
@@ -0,0 +1,5 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "BOARD_NUCLEO_144_WITH_STM32F767ZI")
+    set(BOARD_PATH   "include/boards/board_nucleo_144_with_stm32f767zi")
+    set(MCU_CARD  FALSE)
+    set(SHIELD TRUE)
+endif()
---
+++ bsp/board/include/boards/board_nucleo_144_with_stm32h723zg/board.cmake
@@ -0,0 +1,5 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "BOARD_NUCLEO_144_WITH_STM32H723ZG")
+    set(BOARD_PATH   "include/boards/board_nucleo_144_with_stm32h723zg")
+    set(MCU_CARD  FALSE)
+    set(SHIELD TRUE)
+endif()
