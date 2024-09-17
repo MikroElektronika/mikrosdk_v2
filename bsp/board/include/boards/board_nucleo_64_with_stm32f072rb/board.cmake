eadcac66c6ec8d8dda09ee55efad5fa1cfffe1cc
Ivan Ruzavin <ivan.ruzavin@mikroe.com>
2024-09-17 12:02:25 +0200

    Added release boards to new-feature/boards/10

--- bsp/board/include/boards/board_nucleo_64_with_stm32f072rb/board.cmake
+++ bsp/board/include/boards/board_nucleo_64_with_stm32f072rb/board.cmake
@@ -1,5 +1,5 @@
 if(${_MSDK_BOARD_NAME_} STREQUAL "BOARD_NUCLEO_64_WITH_STM32F072RB")
     set(BOARD_PATH   "include/boards/board_nucleo_64_with_stm32f072rb")
     set(MCU_CARD  FALSE)
-    set(SHIELD FALSE)
+    set(SHIELD TRUE)
 endif()
