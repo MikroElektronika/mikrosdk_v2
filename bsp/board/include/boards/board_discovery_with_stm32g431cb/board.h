1c82abc8fc97d6ac03b0b88a7772ca8e06938d21
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-09-27 15:03:44 +0200

    Added release boards to new-feature/boards/22

--- bsp/board/include/boards/board_discovery_with_stm32g431cb/board.h
+++ bsp/board/include/boards/board_discovery_with_stm32g431cb/board.h
@@ -54,6 +54,54 @@ extern "C"
 #include "hal_target.h"
 
 // Mapping
+#define MOTOR_CONNECTOR_D1      // Pin not routed
+#define MOTOR_CONNECTOR_D2      PC13
+#define MOTOR_CONNECTOR_D3      PC14
+#define MOTOR_CONNECTOR_D4      PC15
+#define MOTOR_CONNECTOR_D5      PF0
+#define MOTOR_CONNECTOR_D6      PF1
+#define MOTOR_CONNECTOR_D7      PG10
+#define MOTOR_CONNECTOR_D8      PA0
+#define MOTOR_CONNECTOR_D9      PA1
+#define MOTOR_CONNECTOR_D10     PA2
+#define MOTOR_CONNECTOR_D11     PA3
+#define MOTOR_CONNECTOR_D12     PA4
+#define MOTOR_CONNECTOR_D13     PA5
+#define MOTOR_CONNECTOR_D14     PA6
+#define MOTOR_CONNECTOR_D15     PA7
+#define MOTOR_CONNECTOR_D16     PC4
+#define MOTOR_CONNECTOR_D17     PB0
+#define MOTOR_CONNECTOR_D18     PB1
+#define MOTOR_CONNECTOR_D19     PB2
+#define MOTOR_CONNECTOR_D20     // Pin not routed
+#define MOTOR_CONNECTOR_D21     // Pin not routed
+#define MOTOR_CONNECTOR_D22     PB10
+#define MOTOR_CONNECTOR_D23     // Pin not routed
+#define MOTOR_CONNECTOR_D24     PB11
+#define MOTOR_CONNECTOR_D25     PB12
+#define MOTOR_CONNECTOR_D26     PB13
+#define MOTOR_CONNECTOR_D27     PB14
+#define MOTOR_CONNECTOR_D28     PB15
+#define MOTOR_CONNECTOR_D29     PC6
+#define MOTOR_CONNECTOR_D30     PA8
+#define MOTOR_CONNECTOR_D31     PA9
+#define MOTOR_CONNECTOR_D32     PA10
+#define MOTOR_CONNECTOR_D33     PA11
+#define MOTOR_CONNECTOR_D34     PA12
+#define MOTOR_CONNECTOR_D35     // Pin not routed
+#define MOTOR_CONNECTOR_D36     PA13
+#define MOTOR_CONNECTOR_D37     PA14
+#define MOTOR_CONNECTOR_D38     PA15
+#define MOTOR_CONNECTOR_D39     PC10
+#define MOTOR_CONNECTOR_D40     PC11
+#define MOTOR_CONNECTOR_D41     PB3
+#define MOTOR_CONNECTOR_D42     PB4
+#define MOTOR_CONNECTOR_D43     PB5
+#define MOTOR_CONNECTOR_D44     PB6
+#define MOTOR_CONNECTOR_D45     PB7
+#define MOTOR_CONNECTOR_D46     PB8
+#define MOTOR_CONNECTOR_D47     PB9
+
 
 #ifdef __cplusplus
 }
