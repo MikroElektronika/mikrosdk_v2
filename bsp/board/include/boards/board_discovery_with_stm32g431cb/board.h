9216b5c371ecd1ebd0277b9a82e75acb0054dcfa
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-02 14:11:05 +0200

    Added release hw to new-feature/boards_cards/3

---
+++ bsp/board/include/boards/board_discovery_with_stm32g431cb/board.h
@@ -0,0 +1,111 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  board.h
+ * @brief Main board pin mapping.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define BOARD_NAME "STM32G431CB_DISCOVERY"
+
+#include "hal_target.h"
+
+// Mapping
+#define MOTOR_CONNECTOR_D1      // Pin not routed
+#define MOTOR_CONNECTOR_D2      PC13
+#define MOTOR_CONNECTOR_D3      PC14
+#define MOTOR_CONNECTOR_D4      PC15
+#define MOTOR_CONNECTOR_D5      PF0
+#define MOTOR_CONNECTOR_D6      PF1
+#define MOTOR_CONNECTOR_D7      PG10
+#define MOTOR_CONNECTOR_D8      PA0
+#define MOTOR_CONNECTOR_D9      PA1
+#define MOTOR_CONNECTOR_D10     PA2
+#define MOTOR_CONNECTOR_D11     PA3
+#define MOTOR_CONNECTOR_D12     PA4
+#define MOTOR_CONNECTOR_D13     PA5
+#define MOTOR_CONNECTOR_D14     PA6
+#define MOTOR_CONNECTOR_D15     PA7
+#define MOTOR_CONNECTOR_D16     PC4
+#define MOTOR_CONNECTOR_D17     PB0
+#define MOTOR_CONNECTOR_D18     PB1
+#define MOTOR_CONNECTOR_D19     PB2
+#define MOTOR_CONNECTOR_D20     // Pin not routed
+#define MOTOR_CONNECTOR_D21     // Pin not routed
+#define MOTOR_CONNECTOR_D22     PB10
+#define MOTOR_CONNECTOR_D23     // Pin not routed
+#define MOTOR_CONNECTOR_D24     PB11
+#define MOTOR_CONNECTOR_D25     PB12
+#define MOTOR_CONNECTOR_D26     PB13
+#define MOTOR_CONNECTOR_D27     PB14
+#define MOTOR_CONNECTOR_D28     PB15
+#define MOTOR_CONNECTOR_D29     PC6
+#define MOTOR_CONNECTOR_D30     PA8
+#define MOTOR_CONNECTOR_D31     PA9
+#define MOTOR_CONNECTOR_D32     PA10
+#define MOTOR_CONNECTOR_D33     PA11
+#define MOTOR_CONNECTOR_D34     PA12
+#define MOTOR_CONNECTOR_D35     // Pin not routed
+#define MOTOR_CONNECTOR_D36     PA13
+#define MOTOR_CONNECTOR_D37     PA14
+#define MOTOR_CONNECTOR_D38     PA15
+#define MOTOR_CONNECTOR_D39     PC10
+#define MOTOR_CONNECTOR_D40     PC11
+#define MOTOR_CONNECTOR_D41     PB3
+#define MOTOR_CONNECTOR_D42     PB4
+#define MOTOR_CONNECTOR_D43     PB5
+#define MOTOR_CONNECTOR_D44     PB6
+#define MOTOR_CONNECTOR_D45     PB7
+#define MOTOR_CONNECTOR_D46     PB8
+#define MOTOR_CONNECTOR_D47     PB9
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // _BOARD_H_
+// ------------------------------------------------------------------------- END
