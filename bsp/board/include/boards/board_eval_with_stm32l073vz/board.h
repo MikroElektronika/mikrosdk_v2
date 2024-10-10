5f462106a2ab1bd6406488ae153b617f1fc1f676
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-10 12:20:40 +0200

    Added release hw to new-feature/boards/37

---
+++ bsp/board/include/boards/board_eval_with_stm32l073vz/board.h
@@ -0,0 +1,116 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  board.h
+ * @brief Main board pin mapping.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define BOARD_NAME "STM32L073VZ-Eval"
+
+#include "hal_target.h"
+
+// Mapping
+#define HEADER_CONNECTOR_CN4_D1 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D2 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D3 	PE7
+#define HEADER_CONNECTOR_CN4_D4 	PE8
+#define HEADER_CONNECTOR_CN4_D5 	PE9
+#define HEADER_CONNECTOR_CN4_D6 	PE10
+#define HEADER_CONNECTOR_CN4_D7 	PE11
+#define HEADER_CONNECTOR_CN4_D8 	PE12
+#define HEADER_CONNECTOR_CN4_D9 	PE13
+#define HEADER_CONNECTOR_CN4_D10	PE14
+#define HEADER_CONNECTOR_CN4_D11	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D12	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D13	PE15
+#define HEADER_CONNECTOR_CN4_D14	PA11
+#define HEADER_CONNECTOR_CN4_D15	PA13
+#define HEADER_CONNECTOR_CN4_D16	PA12
+#define HEADER_CONNECTOR_CN4_D17	PD0
+#define HEADER_CONNECTOR_CN4_D18	PA14
+#define HEADER_CONNECTOR_CN4_D19	PD3
+#define HEADER_CONNECTOR_CN4_D20	PD1
+#define HEADER_CONNECTOR_CN4_D21	PD6
+#define HEADER_CONNECTOR_CN4_D22	PD5
+#define HEADER_CONNECTOR_CN4_D23	PD7
+#define HEADER_CONNECTOR_CN4_D24	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D25	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D26	// Pin not routed
+
+#define HEADER_CONNECTOR_CN5_D1 	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D2 	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D3 	PA5
+#define HEADER_CONNECTOR_CN5_D4 	PB2
+#define HEADER_CONNECTOR_CN5_D5 	PA0
+#define HEADER_CONNECTOR_CN5_D6 	PA4
+#define HEADER_CONNECTOR_CN5_D7 	PH1
+#define HEADER_CONNECTOR_CN5_D8 	PH0
+#define HEADER_CONNECTOR_CN5_D9 	PH10
+#define HEADER_CONNECTOR_CN5_D10	PH9
+#define HEADER_CONNECTOR_CN5_D11	PC14
+#define HEADER_CONNECTOR_CN5_D12	PC15
+#define HEADER_CONNECTOR_CN5_D13	PC13
+#define HEADER_CONNECTOR_CN5_D14	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D15	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D16	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D17	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D18	PE6
+#define HEADER_CONNECTOR_CN5_D19	PE5
+#define HEADER_CONNECTOR_CN5_D20	PE4
+#define HEADER_CONNECTOR_CN5_D21	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D22	PB7
+#define HEADER_CONNECTOR_CN5_D23	PB6
+#define HEADER_CONNECTOR_CN5_D24	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D25	// Pin not routed
+#define HEADER_CONNECTOR_CN5_D26	// Pin not routed
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // _BOARD_H_
+// ------------------------------------------------------------------------- END
---
+++ changelog/new_hw.md
@@ -0,0 +1,30 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+**[BACK TO PREVIOUS FILE](../changelog.md)**
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ [Evaluation board with STM32L073VZ MCU](https://www.st.com/content/st_com/en/products/evaluation-tools/product-evaluation-tools/mcu-mpu-eval-tools/stm32-mcu-mpu-eval-tools/stm32-eval-boards/stm32l073z-eval.html)
++ [Evaluation board with STM32L476ZG MCU](https://www.st.com/content/st_com/en/products/evaluation-tools/product-evaluation-tools/mcu-mpu-eval-tools/stm32-mcu-mpu-eval-tools/stm32-eval-boards/stm32l476g-eval.html)
+
+---
+
+**[BACK TO PREVIOUS FILE](../changelog.md)**
+
+---
