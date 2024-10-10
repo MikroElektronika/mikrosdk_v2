2bfb68d51d1e7193a65c32dafee11d917a7545e0
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-10 12:12:22 +0200

    Added release hw to new-feature/boards/26

--- bsp/board/include/boards/board_discovery_with_stm32f030r8/board.cmake
+++
@@ -1,5 +0,0 @@
-if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_DISCOVERY_WITH_STM32F030R8_MCU")
-    set(BOARD_PATH   "include/boards/board_discovery_with_stm32f030r8")
-    set(MCU_CARD  FALSE)
-    set(SHIELD FALSE)
-endif()
