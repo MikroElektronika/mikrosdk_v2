d9b77a865955e3ead857aaec1d8313987d6677b0
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-10 12:12:51 +0200

    Added release hw to new-feature/boards/27

--- bsp/board/include/boards/board_discovery_with_stm32f100rb/board.cmake
+++
@@ -1,5 +0,0 @@
-if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_DISCOVERY_WITH_STM32F100RB_MCU")
-    set(BOARD_PATH   "include/boards/board_discovery_with_stm32f100rb")
-    set(MCU_CARD  FALSE)
-    set(SHIELD FALSE)
-endif()
