8a08d086cbeca79f9cda87c03e290faaa1a68388
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-10 12:18:55 +0200

    Added release hw to new-feature/boards_cards/9

---
+++ bsp/board/include/boards/board_eval_with_stm32g0c1ve/board.h
@@ -0,0 +1,126 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  board.h
+ * @brief Main board pin mapping.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define BOARD_NAME "STM32G0C1VE-Eval"
+
+#include "hal_target.h"
+
+// Mapping
+#define HEADER_CONNECTOR_CN1_D1 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D2 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D3 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D4 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D5 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D6 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D7 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D8 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D9 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D10	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D11	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D12	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D13	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D14	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D15	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D16	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D17	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D18	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D19	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D20	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D21	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D22	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D23	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D24	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D25	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D26	PA6
+
+#define HEADER_CONNECTOR_CN4_D1 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D2 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D3 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D4 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D5 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D6 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D7 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D8 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D9 	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D10	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D11	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D12	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D13	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D14	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D15	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D16	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D17	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D18	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D19	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D20	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D21	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D22	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D23	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D24	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D25	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D26	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D27	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D28	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D29	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D30	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D31	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D32	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D33	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D34	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D35	// Pin not routed
+#define HEADER_CONNECTOR_CN4_D36	PC0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // _BOARD_H_
+// ------------------------------------------------------------------------- END
