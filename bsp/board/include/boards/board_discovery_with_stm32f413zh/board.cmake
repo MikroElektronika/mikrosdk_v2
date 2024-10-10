09b21bed86426fba5320aefde85f6c8f326dc563
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-10 12:14:44 +0200

    Added release hw to new-feature/boards/29

--- bsp/board/include/boards/board_discovery_with_stm32f413zh/board.cmake
+++
@@ -1,5 +0,0 @@
-if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_DISCOVERY_WITH_STM32F413ZH_MCU")
-    set(BOARD_PATH   "include/boards/board_discovery_with_stm32f413zh")
-    set(MCU_CARD  FALSE)
-    set(SHIELD FALSE)
-endif()
---
+++ bsp/board/include/boards/board_eval_with_stm32f051r8/board.h
@@ -0,0 +1,91 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  board.h
+ * @brief Main board pin mapping.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define BOARD_NAME "STM32F051R8-Eval"
+
+#include "hal_target.h"
+
+// Mapping
+#define HEADER_CONNECTOR_CN1_D1 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D2 	PB14
+#define HEADER_CONNECTOR_CN1_D3 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D4 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D5 	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D6 	PB10
+#define HEADER_CONNECTOR_CN1_D7 	PA8
+#define HEADER_CONNECTOR_CN1_D8 	PB1
+#define HEADER_CONNECTOR_CN1_D9 	PA10
+#define HEADER_CONNECTOR_CN1_D10	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D11	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D12	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D13	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D14	PC4
+#define HEADER_CONNECTOR_CN1_D15	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D16	PA6
+#define HEADER_CONNECTOR_CN1_D17	PA15
+#define HEADER_CONNECTOR_CN1_D18	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D19	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D20	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D21	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D22	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D23	PB3
+#define HEADER_CONNECTOR_CN1_D24	PA2
+#define HEADER_CONNECTOR_CN1_D25	PA1
+#define HEADER_CONNECTOR_CN1_D26	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D27	// Pin not routed
+#define HEADER_CONNECTOR_CN1_D28	PC2
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // _BOARD_H_
+// ------------------------------------------------------------------------- END
