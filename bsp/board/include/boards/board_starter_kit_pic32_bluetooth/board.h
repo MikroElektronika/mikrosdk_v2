add283859d266878467bd1db3e00ff39071b2d1b
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-11-18 09:39:01 +0100

    Added release hw to new-feature/boards/42

---
+++ bsp/board/include/boards/board_starter_kit_pic32_bluetooth/board.h
@@ -0,0 +1,75 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  board.h
+ * @brief Main board pin mapping.
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define BOARD_NAME "PIC32 BLUETOOTH STARTER KIT"
+
+#include "hal_target.h"
+
+// Mapping
+#define HEADER_CONNECTOR_CN1_D3       RC6
+#define HEADER_CONNECTOR_CN1_D4       PB1
+#define HEADER_CONNECTOR_CN1_D5       PC7
+#define HEADER_CONNECTOR_CN1_D6       PC2
+#define HEADER_CONNECTOR_CN1_D7       PB8
+#define HEADER_CONNECTOR_CN1_D8       PB5
+#define HEADER_CONNECTOR_CN1_D9       PB9
+#define HEADER_CONNECTOR_CN1_D10      PB4
+#define HEADER_CONNECTOR_CN1_D11      PA8
+#define HEADER_CONNECTOR_CN1_D12      PB14
+#define HEADER_CONNECTOR_CN1_D13      PA9
+#define HEADER_CONNECTOR_CN1_D14      PC1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // _BOARD_H_
+// ------------------------------------------------------------------------- END
