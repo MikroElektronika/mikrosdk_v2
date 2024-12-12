12e835462fdd2910cb22315c5cda47e2af28883d
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-12-12 12:12:48 +0100

    Added release hw to new-feature/boards/56

---
+++ bsp/board/include/boards/board_launchpad_for_tm4c123g_mcu/board.cmake
@@ -0,0 +1,5 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "BOARD_LAUNCHPAD_FOR_TM4C123G_MCU")
+    set(BOARD_PATH "include/boards/board_launchpad_for_tm4c123g_mcu")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+endif()
