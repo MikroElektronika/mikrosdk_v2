7429f2e25a7f4da5d4ba6b3178ec8da4b0773964
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-12-24 15:13:34 +0100

    Added release hw to new-feature/boards/66

---
+++ bsp/board/include/boards/board_pic24f_curiosity_development/board.cmake
@@ -0,0 +1,5 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "PIC24F_CURIOSITY_DEVELOPMENT_BOARD")
+    set(BOARD_PATH "include/boards/board_pic24f_curiosity_development")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+endif()
