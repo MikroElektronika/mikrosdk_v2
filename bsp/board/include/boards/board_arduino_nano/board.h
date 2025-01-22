e7fbdf1a352aa5dc42082aa37dda845dd907268a
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-01-22 12:42:24 +0100

    Updated header files for PIM and Arduino board

--- bsp/board/include/boards/board_arduino_nano/board.h
+++ bsp/board/include/boards/board_arduino_nano/board.h
@@ -54,12 +54,37 @@ extern "C"
 #include "hal_target.h"
 
 // Mapping
-#define HEADER_CONNECTOR_J1_D1        // Pin not routed
-#define HEADER_CONNECTOR_J1_D2        // Pin not routed
-#define HEADER_CONNECTOR_J1_D3        // Pin not routed
-#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
-#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
-#define HEADER_CONNECTOR_J1_D6        // Pin not routed
+#define HEADER_CONNECTOR_J1_1  GPIO_PD1
+#define HEADER_CONNECTOR_J1_2  GPIO_PD0
+#define HEADER_CONNECTOR_J1_3  // Pin not routed
+#define HEADER_CONNECTOR_J1_4  // Pin not routed
+#define HEADER_CONNECTOR_J1_5  GPIO_PD2
+#define HEADER_CONNECTOR_J1_6  GPIO_PD3
+#define HEADER_CONNECTOR_J1_7  GPIO_PD4
+#define HEADER_CONNECTOR_J1_8  GPIO_PD5
+#define HEADER_CONNECTOR_J1_9  GPIO_PD6
+#define HEADER_CONNECTOR_J1_10 GPIO_PD7
+#define HEADER_CONNECTOR_J1_11 GPIO_PB0
+#define HEADER_CONNECTOR_J1_12 GPIO_PB1
+#define HEADER_CONNECTOR_J1_13 GPIO_PB2
+#define HEADER_CONNECTOR_J1_14 GPIO_PB3
+#define HEADER_CONNECTOR_J1_15 GPIO_PB4
+
+#define HEADER_CONNECTOR_J2_1  // Pin not routed
+#define HEADER_CONNECTOR_J2_2  // Pin not routed
+#define HEADER_CONNECTOR_J2_3  // Pin not routed
+#define HEADER_CONNECTOR_J2_4  // Pin not routed
+#define HEADER_CONNECTOR_J2_5  // Pin not routed
+#define HEADER_CONNECTOR_J2_6  // Pin not routed
+#define HEADER_CONNECTOR_J2_7  GPIO_PC5
+#define HEADER_CONNECTOR_J2_8  GPIO_PC4
+#define HEADER_CONNECTOR_J2_9  GPIO_PC3
+#define HEADER_CONNECTOR_J2_10 GPIO_PC2
+#define HEADER_CONNECTOR_J2_11 GPIO_PC1
+#define HEADER_CONNECTOR_J2_12 GPIO_PC0
+#define HEADER_CONNECTOR_J2_13 // Pin not routed
+#define HEADER_CONNECTOR_J2_14 // Pin not routed
+#define HEADER_CONNECTOR_J2_15 GPIO_PB5
 
 #ifdef __cplusplus
 }
