4bc904bf9cc935d2259690e1b9ece0fca116f900
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-12 09:22:04 +0100

    Added pins for dsPICDEM Connectivity Development Board

--- bsp/board/include/boards/board_dspicdem_net_connectivity_development/board.h
+++ bsp/board/include/boards/board_dspicdem_net_connectivity_development/board.h
@@ -54,12 +54,100 @@ extern "C"
 #include "pim_socket.h"
 
 // Mapping
-#define HEADER_CONNECTOR_J1_D1        // Pin not routed
-#define HEADER_CONNECTOR_J1_D2        // Pin not routed
-#define HEADER_CONNECTOR_J1_D3        // Pin not routed
-#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
-#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
-#define HEADER_CONNECTOR_J1_D6        // Pin not routed
+#define HEADER_J10_1    PIM_PIN_031
+#define HEADER_J10_2    PIM_PIN_083
+#define HEADER_J10_3    PIM_PIN_082
+#define HEADER_J10_4    PIM_PIN_081
+#define HEADER_J10_5    PIM_PIN_080
+#define HEADER_J10_6    PIM_PIN_038
+#define HEADER_J10_7    PIM_PIN_037
+#define HEADER_J10_8    PIM_PIN_036
+#define HEADER_J10_9    PIM_PIN_076
+#define HEADER_J10_10   PIM_PIN_035
+#define HEADER_J10_11   // Pin not routed
+#define HEADER_J10_12   // Pin not routed
+#define HEADER_J10_13   // Pin not routed
+#define HEADER_J10_14   PIM_PIN_090
+#define HEADER_J10_15   PIM_PIN_089
+#define HEADER_J10_16   PIM_PIN_012
+#define HEADER_J10_17   PIM_PIN_010
+#define HEADER_J10_18   PIM_PIN_008
+#define HEADER_J10_19   PIM_PIN_006
+#define HEADER_J10_20   PIM_PIN_004
+#define HEADER_J10_21   PIM_PIN_002
+
+#define HEADER_J11_1    PIM_PIN_014
+#define HEADER_J11_2    PIM_PIN_016
+#define HEADER_J11_3    PIM_PIN_091
+#define HEADER_J11_4    PIM_PIN_092
+#define HEADER_J11_5    PIM_PIN_025
+#define HEADER_J11_6    // Pin not routed
+#define HEADER_J11_7    PIM_PIN_018
+#define HEADER_J11_8    PIM_PIN_020
+#define HEADER_J11_9    PIM_PIN_022
+#define HEADER_J11_10   PIM_PIN_024
+#define HEADER_J11_11   // Pin not routed
+#define HEADER_J11_12   // Pin not routed
+#define HEADER_J11_13   // Pin not routed
+#define HEADER_J11_14   PIM_PIN_026
+#define HEADER_J11_15   PIM_PIN_028
+#define HEADER_J11_16   PIM_PIN_019
+#define HEADER_J11_17   PIM_PIN_017
+#define HEADER_J11_18   PIM_PIN_056
+#define HEADER_J11_19   PIM_PIN_055
+#define HEADER_J11_20   PIM_PIN_048
+#define HEADER_J11_21   PIM_PIN_047
+
+#define HEADER_J12_1    PIM_PIN_049
+#define HEADER_J12_2    PIM_PIN_050
+#define HEADER_J12_3    PIM_PIN_044
+#define HEADER_J12_4    PIM_PIN_045
+#define HEADER_J12_5    PIM_PIN_046
+#define HEADER_J12_6    PIM_PIN_039
+#define HEADER_J12_7    PIM_PIN_040
+#define HEADER_J12_8    // Pin not routed
+#define HEADER_J12_9    // Pin not routed
+#define HEADER_J12_10   // Pin not routed
+#define HEADER_J12_11   // Pin not routed
+#define HEADER_J12_12   // Pin not routed
+#define HEADER_J12_13   PIM_PIN_088
+#define HEADER_J12_14   PIM_PIN_087
+#define HEADER_J12_15   PIM_PIN_062
+#define HEADER_J12_16   PIM_PIN_061
+#define HEADER_J12_17   PIM_PIN_060
+#define HEADER_J12_18   PIM_PIN_059
+#define HEADER_J12_19   PIM_PIN_072
+#define HEADER_J12_20   PIM_PIN_079
+#define HEADER_J12_21   PIM_PIN_078
+
+#define HEADER_J13_1    PIM_PIN_071
+#define HEADER_J13_2    PIM_PIN_070
+#define HEADER_J13_3    PIM_PIN_069
+#define HEADER_J13_4    PIM_PIN_058
+#define HEADER_J13_5    PIM_PIN_057
+#define HEADER_J13_6    PIM_PIN_068
+#define HEADER_J13_7    PIM_PIN_067
+#define HEADER_J13_8    PIM_PIN_066
+#define HEADER_J13_9    PIM_PIN_065
+#define HEADER_J13_10   // Pin not routed
+#define HEADER_J13_11   // Pin not routed
+#define HEADER_J13_12   // Pin not routed
+#define HEADER_J13_13   PIM_PIN_052
+#define HEADER_J13_14   PIM_PIN_051
+#define HEADER_J13_15   PIM_PIN_041
+#define HEADER_J13_16   PIM_PIN_042
+#define HEADER_J13_17   PIM_PIN_094
+#define HEADER_J13_18   PIM_PIN_093
+#define HEADER_J13_19   PIM_PIN_032
+#define HEADER_J13_20   PIM_PIN_034
+#define HEADER_J13_21   PIM_PIN_033
+
+#define BUTTON_1    PIM_PIN_090
+#define BUTTON_2    PIM_PIN_089
+#define BUTTON_3    PIM_PIN_088
+#define LED_1       PIM_PIN_082
+#define LED_2       PIM_PIN_081
+#define LED_3       PIM_PIN_080
 
 #ifdef __cplusplus
 }
