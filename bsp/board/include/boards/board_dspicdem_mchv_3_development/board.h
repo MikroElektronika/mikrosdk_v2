3249fa4f4e4a51d36ba3462912b443961b107047
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-12 09:53:08 +0100

    Added pins

--- bsp/board/include/boards/board_dspicdem_mchv_3_development/board.h
+++ bsp/board/include/boards/board_dspicdem_mchv_3_development/board.h
@@ -54,12 +54,62 @@ extern "C"
 #include "pim_socket.h"
 
 // Mapping
-#define HEADER_CONNECTOR_J1_D1        // Pin not routed
-#define HEADER_CONNECTOR_J1_D2        // Pin not routed
-#define HEADER_CONNECTOR_J1_D3        // Pin not routed
-#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
-#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
-#define HEADER_CONNECTOR_J1_D6        // Pin not routed
+#define HEADER_CONNECTOR_J4_1   // Pin not routed
+#define HEADER_CONNECTOR_J4_2   // Pin not routed
+#define HEADER_CONNECTOR_J4_3   // Pin not routed
+#define HEADER_CONNECTOR_J4_4   // Pin not routed
+#define HEADER_CONNECTOR_J4_5   // Pin not routed
+#define HEADER_CONNECTOR_J4_6   // Pin not routed
+#define HEADER_CONNECTOR_J4_7   // Pin not routed
+#define HEADER_CONNECTOR_J4_8   // Pin not routed
+#define HEADER_CONNECTOR_J4_9   // Pin not routed
+#define HEADER_CONNECTOR_J4_10  // Pin not routed
+#define HEADER_CONNECTOR_J4_11  // Pin not routed
+#define HEADER_CONNECTOR_J4_12  // Pin not routed
+#define HEADER_CONNECTOR_J4_13  // Pin not routed
+#define HEADER_CONNECTOR_J4_14  // Pin not routed
+#define HEADER_CONNECTOR_J4_15  // Pin not routed
+#define HEADER_CONNECTOR_J4_16  // Pin not routed
+#define HEADER_CONNECTOR_J4_17  // Pin not routed
+#define HEADER_CONNECTOR_J4_18  // Pin not routed
+#define HEADER_CONNECTOR_J4_19  // Pin not routed
+#define HEADER_CONNECTOR_J4_20  // Pin not routed
+#define HEADER_CONNECTOR_J4_21  // Pin not routed
+#define HEADER_CONNECTOR_J4_22  // Pin not routed
+#define HEADER_CONNECTOR_J4_23  // Pin not routed
+#define HEADER_CONNECTOR_J4_24  // Pin not routed
+#define HEADER_CONNECTOR_J4_25  // Pin not routed
+#define HEADER_CONNECTOR_J4_26  PIM_PIN_020
+#define HEADER_CONNECTOR_J4_27  PIM_PIN_021
+#define HEADER_CONNECTOR_J4_28  PIM_PIN_022
+#define HEADER_CONNECTOR_J4_29  PIM_PIN_082
+#define HEADER_CONNECTOR_J4_30  PIM_PIN_034
+#define HEADER_CONNECTOR_J4_31  PIM_PIN_077
+#define HEADER_CONNECTOR_J4_32  PIM_PIN_071
+#define HEADER_CONNECTOR_J4_33  PIM_PIN_058
+#define HEADER_CONNECTOR_J4_34  PIM_PIN_059
+#define HEADER_CONNECTOR_J4_35  PIM_PIN_032
+#define HEADER_CONNECTOR_J4_36  PIM_PIN_024
+#define HEADER_CONNECTOR_J4_37  PIM_PIN_025
+#define HEADER_CONNECTOR_J4_38  PIM_PIN_023
+#define HEADER_CONNECTOR_J4_39  PIM_PIN_035
+#define HEADER_CONNECTOR_J4_40  PIM_PIN_043
+#define HEADER_CONNECTOR_J4_41  PIM_PIN_042
+#define HEADER_CONNECTOR_J4_42  PIM_PIN_041
+#define HEADER_CONNECTOR_J4_43  PIM_PIN_029
+#define HEADER_CONNECTOR_J4_44  PIM_PIN_040
+#define HEADER_CONNECTOR_J4_45  PIM_PIN_039
+#define HEADER_CONNECTOR_J4_46  PIM_PIN_038
+#define HEADER_CONNECTOR_J4_47  PIM_PIN_073
+#define HEADER_CONNECTOR_J4_48  PIM_PIN_074
+#define HEADER_CONNECTOR_J4_49  PIM_PIN_066
+#define HEADER_CONNECTOR_J4_50  PIM_PIN_067
+
+#define LED_5   PIM_PIN_093
+#define LED_6   PIM_PIN_094
+#define LED_7   PIM_PIN_098
+#define LED_8   PIM_PIN_099
+#define LED_9   PIM_PIN_100
 
 #ifdef __cplusplus
 }
