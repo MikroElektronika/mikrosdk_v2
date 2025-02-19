2a0820b9b4ebac05b16f7a9b3bc2994e88d07557
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-19 10:55:56 +0100

    paced try inside a loop

--- scripts/check_indexes.py
+++ scripts/check_indexes.py
@@ -68,8 +68,8 @@ if __name__ == "__main__":
 
     err = False
     doc_extra_info = {}
-    try:
-        for indexed_item in es_instance.indexed_items:
+    for indexed_item in es_instance.indexed_items:
+        try:
             if 'download_link_api' in indexed_item['source']:
                 asset_status = requests.get(indexed_item['source']['download_link_api'], headers=headers)
             else:
@@ -121,8 +121,8 @@ if __name__ == "__main__":
                     # Assign correct URLs
                     assign_urls(indexed_item, gh_instance, es_instance)
                 print("%sOK: Asset \"%s\" download link is correct. - %s" % (es_instance.Colors.OKBLUE, indexed_item['source']['name'], indexed_item['source']['download_link']))
-    except Exception as e:
-        print(f'{es_instance.Colors.FAIL}Error for {indexed_item['source']['name']}: {e}')
+        except Exception as e:
+            print(f'{es_instance.Colors.FAIL}Error for {indexed_item['source']['name']}: {e}')
 
         if 'ES_HOST_LEGACY' in os.environ and 'ES_USER_LEGACY' in os.environ and 'ES_PASSWORD_LEGACY':
             if indexed_item['source']['name'] in legacy_packages:
