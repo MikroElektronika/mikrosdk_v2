a648ddb166ff80f7be554a058e1b4814568607c9
Ivan Ruzavin <ivan.ruzavin@mail.ru>
2025-02-20 19:35:10 +0100

    Updated files for release

---
+++ bsp/board/include/boards/board_explorer_8_development_kit/pim_socket/pim_socket.h
@@ -0,0 +1,147 @@
+/****************************************************************************
+**
+** Copyright (C) ${COPYRIGHT_YEAR} MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** OF MERCHANTABILITY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+** TO THE WARRANTIES FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+ * @file  pim_socket.h
+ * @brief PIM socket MCU card pin mapping.
+ */
+
+#ifndef _PIM_SOCKET_H_
+#define _PIM_SOCKET_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "mcu_card.h"
+#include "hal_target.h"
+
+// Left connector
+#define PIM_PIN_01 LEFT_CN_PIN_01
+#define PIM_PIN_02 LEFT_CN_PIN_02
+#define PIM_PIN_03 LEFT_CN_PIN_03
+#define PIM_PIN_04 LEFT_CN_PIN_04
+#define PIM_PIN_05 LEFT_CN_PIN_05
+#define PIM_PIN_06 LEFT_CN_PIN_06
+#define PIM_PIN_07 LEFT_CN_PIN_07
+#define PIM_PIN_08 LEFT_CN_PIN_08
+#define PIM_PIN_09 LEFT_CN_PIN_09
+#define PIM_PIN_10 LEFT_CN_PIN_10
+#define PIM_PIN_11 LEFT_CN_PIN_11
+#define PIM_PIN_12 LEFT_CN_PIN_12
+#define PIM_PIN_13 LEFT_CN_PIN_13
+#define PIM_PIN_14 LEFT_CN_PIN_14
+#define PIM_PIN_15 LEFT_CN_PIN_15
+#define PIM_PIN_16 LEFT_CN_PIN_16
+#define PIM_PIN_17 LEFT_CN_PIN_17
+#define PIM_PIN_18 LEFT_CN_PIN_18
+#define PIM_PIN_19 LEFT_CN_PIN_19
+#define PIM_PIN_20 LEFT_CN_PIN_20
+
+// Down connector
+#define PIM_PIN_21 DOWN_CN_PIN_21
+#define PIM_PIN_22 DOWN_CN_PIN_22
+#define PIM_PIN_23 DOWN_CN_PIN_23
+#define PIM_PIN_24 DOWN_CN_PIN_24
+#define PIM_PIN_25 DOWN_CN_PIN_25
+#define PIM_PIN_26 DOWN_CN_PIN_26
+#define PIM_PIN_27 DOWN_CN_PIN_27
+#define PIM_PIN_28 DOWN_CN_PIN_28
+#define PIM_PIN_29 DOWN_CN_PIN_29
+#define PIM_PIN_30 DOWN_CN_PIN_30
+#define PIM_PIN_31 DOWN_CN_PIN_31
+#define PIM_PIN_32 DOWN_CN_PIN_32
+#define PIM_PIN_33 DOWN_CN_PIN_33
+#define PIM_PIN_34 DOWN_CN_PIN_34
+#define PIM_PIN_35 DOWN_CN_PIN_35
+#define PIM_PIN_36 DOWN_CN_PIN_36
+#define PIM_PIN_37 DOWN_CN_PIN_37
+#define PIM_PIN_38 DOWN_CN_PIN_38
+#define PIM_PIN_39 DOWN_CN_PIN_39
+#define PIM_PIN_40 DOWN_CN_PIN_40
+
+// Right connector
+#define PIM_PIN_41 RIGHT_CN_PIN_41
+#define PIM_PIN_42 RIGHT_CN_PIN_42
+#define PIM_PIN_43 RIGHT_CN_PIN_43
+#define PIM_PIN_44 RIGHT_CN_PIN_44
+#define PIM_PIN_45 RIGHT_CN_PIN_45
+#define PIM_PIN_46 RIGHT_CN_PIN_46
+#define PIM_PIN_47 RIGHT_CN_PIN_47
+#define PIM_PIN_48 RIGHT_CN_PIN_48
+#define PIM_PIN_49 RIGHT_CN_PIN_49
+#define PIM_PIN_50 RIGHT_CN_PIN_50
+#define PIM_PIN_51 RIGHT_CN_PIN_51
+#define PIM_PIN_52 RIGHT_CN_PIN_52
+#define PIM_PIN_53 RIGHT_CN_PIN_53
+#define PIM_PIN_54 RIGHT_CN_PIN_54
+#define PIM_PIN_55 RIGHT_CN_PIN_55
+#define PIM_PIN_56 RIGHT_CN_PIN_56
+#define PIM_PIN_57 RIGHT_CN_PIN_57
+#define PIM_PIN_58 RIGHT_CN_PIN_58
+#define PIM_PIN_59 RIGHT_CN_PIN_59
+#define PIM_PIN_60 RIGHT_CN_PIN_60
+
+// Up connector
+#define PIM_PIN_61 UP_CN_PIN_61
+#define PIM_PIN_62 UP_CN_PIN_62
+#define PIM_PIN_63 UP_CN_PIN_63
+#define PIM_PIN_64 UP_CN_PIN_64
+#define PIM_PIN_65 UP_CN_PIN_65
+#define PIM_PIN_66 UP_CN_PIN_66
+#define PIM_PIN_67 UP_CN_PIN_67
+#define PIM_PIN_68 UP_CN_PIN_68
+#define PIM_PIN_69 UP_CN_PIN_69
+#define PIM_PIN_70 UP_CN_PIN_70
+#define PIM_PIN_71 UP_CN_PIN_71
+#define PIM_PIN_72 UP_CN_PIN_72
+#define PIM_PIN_73 UP_CN_PIN_73
+#define PIM_PIN_74 UP_CN_PIN_74
+#define PIM_PIN_75 UP_CN_PIN_75
+#define PIM_PIN_76 UP_CN_PIN_76
+#define PIM_PIN_77 UP_CN_PIN_77
+#define PIM_PIN_78 UP_CN_PIN_78
+#define PIM_PIN_79 UP_CN_PIN_79
+#define PIM_PIN_80 UP_CN_PIN_80
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _PIM_SOCKET_H_
+// ------------------------------------------------------------------------- END
