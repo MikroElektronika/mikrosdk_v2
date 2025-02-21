7b3f000ce140a5c70b6d7697957cd9f9d483b043
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-21 08:28:01 +0100

    Added MCU_CARD FALSE

--- bsp/board/include/boards/board_explorer_8_development_kit/board.cmake
+++ bsp/board/include/boards/board_explorer_8_development_kit/board.cmake
@@ -1,5 +1,6 @@
 if(${_MSDK_BOARD_NAME_} STREQUAL "EXPLORER_8_DEVELOPMENT_KIT")
     set(BOARD_PATH "include/boards/board_explorer_8_development_kit")
+    set(MCU_CARD FALSE)
     set(PIM_SOCKET TRUE)
     set(SHIELD FALSE)
     set(DIP_SOCKET TRUE)
