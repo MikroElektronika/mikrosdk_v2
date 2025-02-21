d0b9e327740f8d8617e0ff3958a955f1087c7247
Esma <esma.niksic@mikroe.com>
2025-02-21 12:49:30 +0100

    clicker 4 renaming

---
+++ bsp/board/include/boards/clicker_4_for_pic18f97j94/board.cmake
@@ -0,0 +1,4 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "CLICKER_4_FOR_PIC18F97J94")
+    set(BOARD_PATH   "include/boards/clicker_4_for_pic18f97j94")
+    set(MCU_CARD  FALSE)
+endif()
---
+++ bsp/board/include/boards/clicker_4_for_stm32f745vg/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "CLICKER_4_FOR_STM32F745VG")
+    set(BOARD_PATH "include/boards/clicker_4_for_stm32f745vg")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
