90178324ea7c5c8edefa74f783006cce0942d66d
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-21 10:22:40 +0100

    Added release hw to new-feature/boards/103

---
+++ bsp/board/include/boards/board_microstick_ii/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "MICROSTICK_II_BOARD")
+    set(BOARD_PATH "include/boards/board_microstick_ii")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET TRUE)
+endif()
