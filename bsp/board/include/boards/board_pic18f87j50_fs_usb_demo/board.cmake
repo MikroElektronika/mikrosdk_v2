55513e3348855eee9345d5b0e8717b0e721cbbd3
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-02-27 10:31:10 +0100

    Added release hw to new-feature/boards/110

---
+++ bsp/board/include/boards/board_pic18f87j50_fs_usb_demo/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "PIC18F87J50_FS_USB_DEMO_BOARD")
+    set(BOARD_PATH "include/boards/board_pic18f87j50_fs_usb_demo")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
