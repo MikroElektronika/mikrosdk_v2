472cf1848b541cb3b699d729691e62fd91688c3b
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-25 13:55:54 +0100

    Updated pins

--- bsp/board/include/boards/board_xmega_e5_xplained/board.h
+++ bsp/board/include/boards/board_xmega_e5_xplained/board.h
@@ -54,12 +54,47 @@ extern "C"
 #include "hal_target.h"
 
 // Mapping
-#define HEADER_CONNECTOR_J1_D1        // Pin not routed
-#define HEADER_CONNECTOR_J1_D2        // Pin not routed
-#define HEADER_CONNECTOR_J1_D3        // Pin not routed
-#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
-#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
-#define HEADER_CONNECTOR_J1_D6        // Pin not routed
+#define J1_1    GPIO_PC0
+#define J1_2    GPIO_PC1
+#define J1_3    GPIO_PC2
+#define J1_4    GPIO_PC3
+#define J1_5    GPIO_PC4
+#define J1_6    GPIO_PC7
+#define J1_7    GPIO_PC6
+#define J1_8    GPIO_PC5
+
+#define J2_1    GPIO_PA0
+#define J2_2    GPIO_PA1
+#define J2_3    GPIO_PA2
+#define J2_4    GPIO_PA3
+#define J2_5    GPIO_PA4
+#define J2_6    GPIO_PA5
+#define J2_7    GPIO_PA6
+#define J2_8    GPIO_PA7
+
+#define J3_1    // Pin not routed
+#define J3_2    // Pin not routed
+#define J3_3    // Pin not routed
+#define J3_4    // Pin not routed
+#define J3_5    GPIO_PC4
+#define J3_6    GPIO_PC7
+#define J3_7    GPIO_PC6
+#define J3_8    GPIO_PC5
+
+#define J4_1    GPIO_PD0
+#define J4_2    GPIO_PD1
+#define J4_3    GPIO_PD2
+#define J4_4    GPIO_PD3
+#define J4_5    GPIO_PD4
+#define J4_6    GPIO_PD7
+#define J4_7    GPIO_PD6
+#define J4_8    GPIO_PD5
+
+#define LED_0   GPIO_PD4
+#define LED_1   GPIO_PD5
+
+#define BUTTON_1    GPIO_PD0
+#define BUTTON_2    GPIO_PD2
 
 #ifdef __cplusplus
 }
