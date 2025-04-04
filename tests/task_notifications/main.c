4839b7252c6b2f30a1aa563b570f37781857cf76
martina <martinajelesic@gmail.com>
2025-04-04 11:33:21 +0200

    new example added

---
+++ tests/task_notifications/main.c
@@ -0,0 +1,77 @@
+#include "board.h"
+#include "drv_digital_out.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "portmacro.h"
+#include "semphr.h"
+#include "systick.h"
+#include <sys/time.h>
+#include "drv_port.h"
+
+static digital_out_t pinA;
+static digital_out_t pinB;
+static digital_out_t pinC;
+static digital_out_t pinD;
+static digital_out_t pinE;
+static digital_out_t pinG;
+static digital_out_t pinF;
+static SemaphoreHandle_t  sema; 
+static SemaphoreHandle_t  sema2; 
+static port_t portD;
+static TaskHandle_t handle1;
+static TaskHandle_t handle2;
+int number=0;
+static void f(void* param)
+{
+    while(1){
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        port_write(&portD, 0xff);
+        vTaskDelay(100);
+        xTaskNotifyGive(handle2);
+    }
+}
+static void f2(void* param)
+{
+    uint8_t elem;
+    while(1){
+        port_write(&portD, 0);
+        vTaskDelay(50);
+        xTaskNotifyGive(handle1);
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    }
+}
+
+#define NVIC_ISER0    (*(volatile uint32_t*)0xE000E100)
+
+void TIM2_Init(void) {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    uint32_t prescaler = 90000 - 1;  
+    TIM2->PSC = prescaler;  
+    TIM2->ARR = 999;  
+    TIM2->DIER |= TIM_DIER_UIE;
+    TIM2->CR1 |= TIM_CR1_CEN;
+    NVIC_ISER0 |= (1 << 25);
+}
+__attribute__ ((interrupt("IRQ"))) void TIM2_UP_TIM20_IRQHandler(void) {
+    if (TIM2->SR & TIM_SR_UIF) {
+        TIM2->SR &= ~TIM_SR_UIF;
+    }
+}
+size_t val;
+#define NVIC_SYSTICK  (*(volatile uint32_t*)0xE000E40C)
+int main(){
+    #ifdef PREINIT_SUPPORTED
+    preinit();
+    #endif
+    __asm volatile ("cpsie i");
+    TIM2_Init();
+    port_init(&portD,PORT_D,0xff, PIN_DIRECTION_DIGITAL_OUTPUT);
+    if(xTaskCreate(f, "task1", 128, NULL, 4, &handle1)==pdPASS){
+            if(xTaskCreate(f2, "task2", 128, NULL, 4, &handle2)==pdFALSE){
+                // Enter an infinite loop to halt the system
+                while(1);
+            } 
+        vTaskStartScheduler();
+    }
+    while(1);
+}
