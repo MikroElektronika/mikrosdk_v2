bf0ac539ae680167459ca6549da975983746410b
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-04-04 08:33:04 +0200

    Updated pins

--- bsp/board/include/boards/board_curiosity_nano_with_pic18f57q84/board.h
+++ bsp/board/include/boards/board_curiosity_nano_with_pic18f57q84/board.h
@@ -54,12 +54,84 @@ extern "C"
 #include "hal_target.h"
 
 // Mapping
-#define HEADER_CONNECTOR_J1_D1        // Pin not routed
-#define HEADER_CONNECTOR_J1_D2        // Pin not routed
-#define HEADER_CONNECTOR_J1_D3        // Pin not routed
-#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
-#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
-#define HEADER_CONNECTOR_J1_D6        // Pin not routed
+// Left connector
+#define LEFT_CN_PIN_001 GPIO_PC7
+#define LEFT_CN_PIN_002 GPIO_PD4
+#define LEFT_CN_PIN_003 GPIO_PD5
+#define LEFT_CN_PIN_004 GPIO_PD6
+#define LEFT_CN_PIN_005 GPIO_PD7
+#define LEFT_CN_PIN_008 GPIO_PB0
+#define LEFT_CN_PIN_009 GPIO_PB1
+#define LEFT_CN_PIN_010 GPIO_PB2
+#define LEFT_CN_PIN_011 GPIO_PB3
+#define LEFT_CN_PIN_012 GPIO_PF4 // Pin not routed to mikroBUS
+
+// Down connector
+#define DOWN_CN_PIN_013 GPIO_PF5 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_014 GPIO_PF6 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_015 GPIO_PF7 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_016 GPIO_PB4 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_017 GPIO_PB5
+#define DOWN_CN_PIN_018 GPIO_PB6 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_019 GPIO_PB7 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_020 GPIO_PE3 // Pin not routed to mikroBUS
+#define DOWN_CN_PIN_021 GPIO_PA0
+#define DOWN_CN_PIN_022 GPIO_PA1
+#define DOWN_CN_PIN_023 GPIO_PA2
+#define DOWN_CN_PIN_024 GPIO_PA3
+
+// Right connector
+#define RIGHT_CN_PIN_025 GPIO_PA4
+#define RIGHT_CN_PIN_026 GPIO_PA5
+#define RIGHT_CN_PIN_027 GPIO_PE0 // Pin not routed to mikroBUS
+#define RIGHT_CN_PIN_028 GPIO_PE1 // Pin not routed to mikroBUS
+#define RIGHT_CN_PIN_029 GPIO_PE2
+#define RIGHT_CN_PIN_032 GPIO_PA7
+#define RIGHT_CN_PIN_033 GPIO_PA6
+#define RIGHT_CN_PIN_034 GPIO_PC0 // Pin not routed to mikroBUS
+#define RIGHT_CN_PIN_035 GPIO_PC1 // Pin not routed to mikroBUS
+#define RIGHT_CN_PIN_036 GPIO_PF0 // Pin not routed to mikroBUS
+
+// Up connector
+#define UP_CN_PIN_037 GPIO_PF1 // Pin not routed to mikroBUS
+#define UP_CN_PIN_038 GPIO_PF2 // Pin not routed to mikroBUS
+#define UP_CN_PIN_039 GPIO_PF3 // Pin not routed to mikroBUS
+#define UP_CN_PIN_040 GPIO_PC2
+#define UP_CN_PIN_041 GPIO_PC3
+#define UP_CN_PIN_042 GPIO_PD0 // Pin not routed to mikroBUS
+#define UP_CN_PIN_043 GPIO_PD1 // Pin not routed to mikroBUS
+#define UP_CN_PIN_044 GPIO_PD2 // Pin not routed to mikroBUS
+#define UP_CN_PIN_045 GPIO_PD3 // Pin not routed to mikroBUS
+#define UP_CN_PIN_046 GPIO_PC4
+#define UP_CN_PIN_047 GPIO_PC5
+#define UP_CN_PIN_048 GPIO_PC6
+
+// Header connector
+#define HEADER_CONNECTOR_HC1_PIN7  UP_CN_PIN_040
+#define HEADER_CONNECTOR_HC1_PIN8  UP_CN_PIN_041
+#define HEADER_CONNECTOR_HC1_PIN9  LEFT_CN_PIN_010
+#define HEADER_CONNECTOR_HC1_PIN10 LEFT_CN_PIN_009
+#define HEADER_CONNECTOR_HC1_PIN11 UP_CN_PIN_046
+#define HEADER_CONNECTOR_HC1_PIN12 UP_CN_PIN_047
+#define HEADER_CONNECTOR_HC1_PIN13 UP_CN_PIN_048
+#define HEADER_CONNECTOR_HC1_PIN14 LEFT_CN_PIN_002
+#define HEADER_CONNECTOR_HC1_PIN16 DOWN_CN_PIN_024
+#define HEADER_CONNECTOR_HC1_PIN17 RIGHT_CN_PIN_025
+#define HEADER_CONNECTOR_HC1_PIN18 RIGHT_CN_PIN_029
+#define HEADER_CONNECTOR_HC1_PIN19 LEFT_CN_PIN_001
+
+#define HEADER_CONNECTOR_HC2_PIN7  RIGHT_CN_PIN_032
+#define HEADER_CONNECTOR_HC2_PIN8  RIGHT_CN_PIN_033
+#define HEADER_CONNECTOR_HC2_PIN9  RIGHT_CN_PIN_026
+#define HEADER_CONNECTOR_HC2_PIN10 LEFT_CN_PIN_011
+#define HEADER_CONNECTOR_HC2_PIN11 LEFT_CN_PIN_008
+#define HEADER_CONNECTOR_HC2_PIN12 DOWN_CN_PIN_023
+#define HEADER_CONNECTOR_HC2_PIN13 DOWN_CN_PIN_022
+#define HEADER_CONNECTOR_HC2_PIN14 DOWN_CN_PIN_021
+#define HEADER_CONNECTOR_HC2_PIN16 LEFT_CN_PIN_005
+#define HEADER_CONNECTOR_HC2_PIN17 LEFT_CN_PIN_004
+#define HEADER_CONNECTOR_HC2_PIN18 LEFT_CN_PIN_003
+#define HEADER_CONNECTOR_HC2_PIN19 DOWN_CN_PIN_017
 
 #ifdef __cplusplus
 }
