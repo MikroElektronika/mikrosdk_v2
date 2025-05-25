140a78aa482d2b23b4470d269e36972ad2c18bbd
lukaklasnic <klasnicluka00@gmail.com>
2025-05-25 16:17:50 +0200

    Update middleware/ft800/lib/include/ft800.h
    Co-authored-by: Ivan Ruzavin <ivan.ruzavin@mikroe.com>

--- middleware/ft800/lib/include/ft800.h
+++ middleware/ft800/lib/include/ft800.h
@@ -1178,8 +1178,8 @@ void cmd_keys(ft800_t *ctx, ft800_cfg_t *cfg,uint16_t* cmdOffset,uint16_t x,uint
 void cmd_progress( ft800_t *ctx, ft800_cfg_t *cfg, uint16_t *cmdOffset, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t options, uint16_t val, uint16_t range );
 
 /**
- * @brief Command Keys.
- * @details This function send command to FT800 co-processor for drawning in-built keys widget.
+ * @brief Send Command Slider Function.
+ * @details This function sends command to FT800 co-processor for drawing in-built slider widget.
  * @param[in] ctx : FT800 context object. See #ft800_t structure definition
  * for detailed explanation.
  * @param[in] cfg : FT800 configuration object. See #ft800_cfg_t structure
