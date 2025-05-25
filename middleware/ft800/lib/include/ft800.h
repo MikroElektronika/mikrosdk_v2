d217b3a49c0cfc725caf716b81282f8d85cd849e
lukaklasnic <klasnicluka00@gmail.com>
2025-05-25 16:28:22 +0200

    Update middleware/ft800/lib/include/ft800.h
    Co-authored-by: Ivan Ruzavin <ivan.ruzavin@mikroe.com>

--- middleware/ft800/lib/include/ft800.h
+++ middleware/ft800/lib/include/ft800.h
@@ -1290,8 +1290,8 @@ void cmd_scrollbar( ft800_t *ctx, ft800_cfg_t *cfg, uint16_t *cmdOffset, uint16_
 void cmd_dial( ft800_t *ctx, ft800_cfg_t *cfg, uint16_t *cmdOffset, uint16_t x, uint16_t y, uint16_t r, uint16_t options, uint16_t val );
 
 /**
- * @brief Command Toggle.
- * @details This function send command to FT800 co-processor for drawning in-built toggle widget.
+ * @brief Send Command Toggle Function.
+ * @details This function sends command to FT800 co-processor for drawing in-built toggle widget.
  * @param[in] ctx : FT800 context object. See #ft800_t structure definition
  * for detailed explanation.
  * @param[in] cfg : FT800 configuration object. See #ft800_cfg_t structure
