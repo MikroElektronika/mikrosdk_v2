5af839fa9c59493b99fc88f4ec9f558a37caecba
lukaklasnic <klasnicluka00@gmail.com>
2025-05-25 16:29:10 +0200

    Update middleware/ft800/lib/include/ft800.h
    Co-authored-by: Ivan Ruzavin <ivan.ruzavin@mikroe.com>

--- middleware/ft800/lib/include/ft800.h
+++ middleware/ft800/lib/include/ft800.h
@@ -1327,8 +1327,8 @@ void cmd_dial( ft800_t *ctx, ft800_cfg_t *cfg, uint16_t *cmdOffset, uint16_t x, 
 void cmd_toggle( ft800_t *ctx, ft800_cfg_t *cfg, uint16_t *cmdOffset, uint16_t x, uint16_t y, uint16_t w, uint16_t font, uint16_t options, uint16_t state, const char *s );
 
 /**
- * @brief Command Track.
- * @details This function send command to FT800 co-processor to start tracking movement of press
+ * @brief Send Command Track Function.
+ * @details This function sends command to FT800 co-processor to start tracking movement of press
  * applied at drawned object after this function.
  * @param[in] ctx : FT800 context object. See #ft800_t structure definition
  * for detailed explanation.
