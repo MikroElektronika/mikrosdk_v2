a6cb9b6abd50abcd1c4e9a0eabad1d29b4859e4b
esmaniksic <esma.niksic@mikroe.com>
2025-06-30 08:01:55 +0200

    gpio test wip

--- tests/gpio/src/main.c
+++ tests/gpio/src/main.c
@@ -24,8 +24,8 @@
 
 // -------------------------------------------------------------------- MACROS
 // TODO
-#define TEST_CLOCK        true
-#define CLOCK_TEST_PORT   HAL_PORT_NC
+#define TEST_CLOCK        false
+#define CLOCK_TEST_PORT   PORT_4
 
 #define TEST_FLATTENER    false
 
@@ -38,7 +38,7 @@
 #else
 #define PIN_TEST          false
 #define PORT_TEST         false
-#define BUTTON_TEST       false
+#define BUTTON_TEST       true
 #endif
 // TODO
 // Define port used for signaling errors.
@@ -61,20 +61,24 @@
 
 // TODO
 // Define port used for testing.
-#define PORT_NAME HAL_PORT_NC // Example: GPIO_PORT_B
+#define PORT_NAME PORT_4 // Example: GPIO_PORT_B
 #define PORT_MASK (port_size_t)0xFFFFFFFF
 #define PORT_READ_VALUE 0xAA
 #define SINGLE_LED_DELAY 300  // Delay LED single shift test.
 // TODO
 // Define pins used for testing digital in/out.
-#define LED HAL_PIN_NC        // Example: GPIO_PA0
-#define BUTTON HAL_PIN_NC     // Example: GPIO_PB0
+#define LED GPIO_P409        // Example: GPIO_PA0
+#define BUTTON GPIO_P301     // Example: GPIO_PB0
 // ----------------------------------------------------------------- VARIABLES
 static port_t test_port;          // PORT driver context structure.
 static digital_in_t input_pin;    // Digital input driver context structure.
 static digital_out_t output_pin;  // Digital output driver context structure.
 
 static uint8_t port_counter = port_count_size;  // Defined in CMakeLists.txt file.
+
+#define PWPR_REGISTER_BASE (*( volatile uint8_t * )0x40040D03UL)
+#include "mcu.h"
+
 // ----------------------------------------------------------------- USER CODE
 int main( void ) {
     /* Do not remove this line or clock might not be set correctly. */
@@ -82,11 +86,41 @@ int main( void ) {
     preinit();
     #endif
 
+
+    // // Clear the B0WI bit in the PWPR register. This enables writing to the PFSWE bit in the PWPR register.
+    // // PWPR_REGISTER_BASE &= ~0x80; // Clear B0WI bit
+    // R_PMISC->PWPR_b.B0WI = 0;
+    // // Set 1 to the PFSWE bit in the PWPR register. This enables writing to the PmnPFS register.
+    // // PWPR_REGISTER_BASE |= 0x40; // Set PFSWE bit
+    // R_PMISC->PWPR_b.PFSWE = 1;
+
+    // // R_PFS->PORT[0].PIN[0].PmnPFS_b.PDR = 1;
+    // // R_PFS->PORT[0].PIN[0].PmnPFS_b.PMR = 0;
+    // // R_PFS->PORT[0].PIN[0].PmnPFS_b.PODR = 1;
+
+    // R_PFS->PORT[0].PIN[0].PmnPFS = 0x00000000; // GPIO mode
+    // R_PORT0->PDR_b.PDR0 = 1; // Direction: output
+    // R_PORT0->PODR_b.PODR0 = 0; // Initial state: low
+    // R_PORT0->PODR |= 1;
+    // while(1);
+
+    // // Clear the PFSWE bit in the PWPR register. This disables writing to the PmnPFS register.
+    // R_PMISC->PWPR_b.PFSWE = 0;
+    // // Set 1 to the B0WI bit in the PWPR register. This disables writing to the PFSWE bit in the PWPR register
+    // R_PMISC->PWPR_b.B0WI = 1;
+
     volatile pin_name_t fetch_pin = hal_gpio_fetch_pin(LED);
     volatile port_name_t fetch_port = hal_gpio_fetch_port(LED);
 
     #if TEST_CLOCK
-    CLOCK_TEST( CLOCK_TEST_PORT );
+    // CLOCK_TEST( CLOCK_TEST_PORT );
+    port_init( &test_port, CLOCK_TEST_PORT, 0xFFFF, PIN_DIRECTION_DIGITAL_OUTPUT ); \
+                      while(1) { \
+                          port_write( &test_port, 0xFFFF ); \
+                          Delay_1sec(); \
+                          port_write( &test_port, 0x00 ); \
+                          Delay_1sec(); \
+                      }
     #endif
 
     #if TEST_FLATTENER
