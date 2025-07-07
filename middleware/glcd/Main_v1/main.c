c75e94ecf8a42bfa36af1888cda9841e1b36a269
rabmarac <uros.krsteski@gmail.com>
2025-07-07 15:03:40 +0200

    Test filled function with various polygons

--- middleware/glcd/Main_v1/main.c
+++ middleware/glcd/Main_v1/main.c
@@ -24,13 +24,35 @@ int main(void)
     GLCD_Display(&glcd, on);        // Turn on the GLCD
     GLCD_Clear(&glcd);              // Clear the GLCD
 
-    point p2[] = { { 0, 0 }, { 64, 60 }, { 12, 60 }, { 112, 40 }, {12 , 32} };
-    point p = {0, 0};
-    point p3[] = { { 0, 0 }, { 64, 0 } };
+    point p[] = { { 0, 0 }, { 64, 60 }, { 12, 60 }, { 112, 40 }, {12 , 32} };
+    point p2[] = { { 0, 0 }, { 64, 60 }, { 12, 60 }, { 112, 40 } };
+    point p3[] = { { 0, 0 }, { 64, 60 }, { 12, 60 } };
+    
+    uint8_t psize = sizeof(p) / sizeof(p[0]);
     uint8_t p2size = sizeof(p2) / sizeof(p2[0]);
+    uint8_t p3size = sizeof(p3) / sizeof(p3[0]);
     while (1) 
     {
-        GLCD_Draw_Rect_Giving_Size(&glcd, &p, 90, 30, 2, false, false);
+        GLCD_Draw_Polygon(&glcd, p, psize, 2, false, false);
+        Delay_ms(1000);
+
+        GLCD_Draw_Polygon(&glcd, p, psize, 2, true, false);
+        Delay_ms(1000);
+        GLCD_Clear(&glcd);
+
+        GLCD_Draw_Polygon(&glcd, p2, p2size, 2, false, false);
+        Delay_ms(1000);
+
+        GLCD_Draw_Polygon(&glcd, p2, p2size, 2, true, false);
+        Delay_ms(1000);
+        GLCD_Clear(&glcd);
+
+        GLCD_Draw_Polygon(&glcd, p3, p3size, 2, false, false);
+        Delay_ms(1000);
+
+        GLCD_Draw_Polygon(&glcd, p3, p3size, 2, true, false);
+        Delay_ms(1000);
+        GLCD_Clear(&glcd);
     }
     return 0;
 }
