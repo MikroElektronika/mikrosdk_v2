988307754be7ffabb8dc65f196d5064c9845ee75
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-08-05 08:26:49 +0200

    Added release hw to new-feature/boards/0508

---
+++ bsp/board/include/boards/board_nucleo_144/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "NUCLEO_144_WITH_STM32H755ZI_MCU")
+    set(BOARD_PATH "include/boards/board_nucleo_144")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
