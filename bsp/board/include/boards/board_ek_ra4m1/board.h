3058233ce43e5227a88b6ec54ddee8ccf7b15889
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-08-07 10:35:14 +0200

    Added release hw to new-feature/boards/1208

--- bsp/board/include/boards/board_ek_ra4m1/board.h
+++ bsp/board/include/boards/board_ek_ra4m1/board.h
@@ -51,8 +51,15 @@ extern "C"
 
 #define BOARD_NAME "Evaluation Kit for RA4M1 MCU Group"
 
+#include "hal_target.h"
+
 // Mapping
-// Only Bare Metal support is available for this board at the moment
+#define HEADER_CONNECTOR_J1_D1        // Pin not routed
+#define HEADER_CONNECTOR_J1_D2        // Pin not routed
+#define HEADER_CONNECTOR_J1_D3        // Pin not routed
+#define HEADER_CONNECTOR_J1_D4        GPIO_PB7
+#define HEADER_CONNECTOR_J1_D5        GPIO_PB6
+#define HEADER_CONNECTOR_J1_D6        // Pin not routed
 
 #ifdef __cplusplus
 }
