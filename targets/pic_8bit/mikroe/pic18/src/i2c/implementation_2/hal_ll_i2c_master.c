f652fbf5d99ca252120a1c3030d8381ca6a09e12
Strahinja Jacimovic <strahinja.jacimovic@mikroe.com>
2025-09-03 09:33:17 +0200

    Update targets/pic_8bit/mikroe/pic18/src/i2c/implementation_2/hal_ll_i2c_master.c
    Co-authored-by: Ivan Ruzavin <ivan.ruzavin@mikroe.com>

--- targets/pic_8bit/mikroe/pic18/src/i2c/implementation_2/hal_ll_i2c_master.c
+++ targets/pic_8bit/mikroe/pic18/src/i2c/implementation_2/hal_ll_i2c_master.c
@@ -1150,7 +1150,7 @@ static void hal_ll_i2c_set_baud_rate( hal_ll_i2c_hw_specifics_map_t *map ) {
         clear_reg( hal_ll_hw_reg->i2c_con2_reg_addr );
         // Clock Reference Output
         set_reg_bits( hal_ll_hw_reg->i2c_clk_reg_addr, HAL_LL_I2C_MASTER_CLKR_CLK_SOURCE );
-    } else if( map->speed == HAL_LL_I2C_MASTER_SPEED_1M ) {
+    } else if ( map->speed == HAL_LL_I2C_MASTER_SPEED_1M ) {
         uint8_t oscfrq = (uint8_t)read_reg( OSCFRQ_REG_ADDRESS & HAL_LL_OSCFRQ_MASK );
 
         set_reg_bit( CLKRCON_REG_ADDRESS, HAL_LL_CLKRCON_ENABLE_BIT ); // CLKR enable
