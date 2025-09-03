348ddf481b72860beac8066798a8373b099789ab
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-09-03 07:53:19 +0200

    Added release hw to new-feature/boards/1510

---
+++ bsp/board/include/boards/board_freedom_for_ke1x/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "BOARD_FREEDOM_FOR_KE1X")
+    set(BOARD_PATH "include/boards/board_freedom_for_ke1x")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
