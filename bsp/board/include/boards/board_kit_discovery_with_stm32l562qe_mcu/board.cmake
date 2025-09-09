e320cc63d0a45123e325181a6de38712e33be85c
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-09-09 07:46:01 +0200

    Merged release files from multiple branches

---
+++ bsp/board/include/boards/board_kit_discovery_with_stm32l562qe_mcu/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_DISCOVERY_WITH_STM32L562QE_MCU")
+    set(BOARD_PATH "include/boards/board_kit_discovery_with_stm32l562qe_mcu")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
