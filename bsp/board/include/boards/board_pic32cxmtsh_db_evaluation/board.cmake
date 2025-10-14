704478c32393c67f7bdf230bc088766b81b9c060
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-10-14 15:24:43 +0200

    Added release hw to new-feature/boards/0412

---
+++ bsp/board/include/boards/board_pic32cxmtsh_db_evaluation/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "PIC32CXMTSH_DB_EVALUATION_KIT")
+    set(BOARD_PATH "include/boards/board_pic32cxmtsh_db_evaluation")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
