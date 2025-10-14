a11005ec0341639869ca8f9e1f394bf2399af8d5
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-10-14 15:23:26 +0200

    Added release hw to new-feature/boards/0311

---
+++ bsp/board/include/boards/board_samc21n_xplained_pro_eval/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "SAMC21N_XPLAINED_PRO_EVALUATION_KIT")
+    set(BOARD_PATH "include/boards/board_samc21n_xplained_pro_eval")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
