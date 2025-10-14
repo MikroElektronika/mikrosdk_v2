3acae8ee81a2bbec4b30fce2fc92705ea92ff6c9
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-10-14 15:24:40 +0200

    Added release hw to new-feature/boards/0312

---
+++ bsp/board/include/boards/board_pic32cxmtg_ek_evaluation/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "PIC32CXMTG_EK_EVALUATION_KIT")
+    set(BOARD_PATH "include/boards/board_pic32cxmtg_ek_evaluation")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
