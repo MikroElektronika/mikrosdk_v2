75bbb27095a099fa75bcce17304500ff233940d5
esmaniksic <esma.niksic@mikroe.com>
2025-10-28 09:12:41 +0100

    Update targets/arm/mikroe/common/include/mcu_reg_addresses/TOSHIBA/TMPM4KNFYAFG/mcu_reg_addresses.h
    Co-authored-by: Ivan Ruzavin <ivan.ruzavin@mikroe.com>

--- targets/arm/mikroe/common/include/mcu_reg_addresses/TOSHIBA/TMPM4KNFYAFG/mcu_reg_addresses.h
+++ targets/arm/mikroe/common/include/mcu_reg_addresses/TOSHIBA/TMPM4KNFYAFG/mcu_reg_addresses.h
@@ -39,13 +39,13 @@
 /*!
 * @file  mcu_reg_addresses.h
 * @brief MCU specific register address definitions.
-* @details TMPM4KNF10AFG register address definitions.
+* @details TMPM4KNFYAFG register address definitions.
 */
 
 #ifndef _MCU_REG_ADDRESSES_H_
 #define _MCU_REG_ADDRESSES_H_
 
-// TMPM4KNF10AFG REGISTER DEFINITIONS
+// TMPM4KNFYAFG REGISTER DEFINITIONS
 // EOF REGISTER DEFINITIONS
 
 #endif // _MCU_REG_ADDRESSES_H_
