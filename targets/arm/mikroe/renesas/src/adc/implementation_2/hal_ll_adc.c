5cbb6aa6e1292f4256f2826ae7b848a9a638c5a5
mark0marko <simicmarko790@gmail.com>
2025-12-02 11:10:20 +0100

    correct adc channel selection

--- targets/arm/mikroe/renesas/src/adc/implementation_2/hal_ll_adc.c
+++ targets/arm/mikroe/renesas/src/adc/implementation_2/hal_ll_adc.c
@@ -519,10 +519,15 @@ static void hal_ll_adc_hw_init( hal_ll_adc_hw_specifics_map_t *map ) {
             set_reg_bit( &base->adansa[0], map->channel );
         else if( 16 <= map->channel && 20 >= map->channel )
             set_reg_bit( &base->adansa[1], map->channel - 16 );
-    #elif (defined(R7FA4M3) || defined(R7FA6E2))
+    #elif defined(R7FA4M3)
         if( 0 <= map->channel && 13 >= map->channel )
             set_reg_bit( &base->adansa[0], map->channel );
         else if( 16 <= map->channel && 22 >= map->channel )
+            set_reg_bit( &base->adansa[1], map->channel - 16 );
+    #elif defined(R7FA6E2)
+        if( 0 <= map->channel && 15 >= map->channel )
+            set_reg_bit( &base->adansa[0], map->channel );
+        else if( 16 <= map->channel && 31 >= map->channel )
             set_reg_bit( &base->adansa[1], map->channel - 16 );
     #endif
 
