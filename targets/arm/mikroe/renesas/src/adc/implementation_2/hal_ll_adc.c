994dcce2e50df1d5636d415c27c1c15ebb38c60c
mark0marko <simicmarko790@gmail.com>
2025-12-23 15:33:18 +0100

    adc implementation update

--- targets/arm/mikroe/renesas/src/adc/implementation_2/hal_ll_adc.c
+++ targets/arm/mikroe/renesas/src/adc/implementation_2/hal_ll_adc.c
@@ -524,6 +524,11 @@ static void hal_ll_adc_hw_init( hal_ll_adc_hw_specifics_map_t *map ) {
             set_reg_bit( &base->adansa[0], map->channel );
         else if( 16 <= map->channel && 22 >= map->channel )
             set_reg_bit( &base->adansa[1], map->channel - 16 );
+    #elif defined(R7FA6M5)
+        if( 0 <= map->channel && 13 >= map->channel )
+            set_reg_bit( &base->adansa[0], map->channel );
+        else if( 16 <= map->channel && 28 >= map->channel )
+            set_reg_bit( &base->adansa[1], map->channel - 16 );
     #elif defined(R7FA2E3)
         if( 0 <= map->channel && 10 >= map->channel )
             set_reg_bit( &base->adansa[0], map->channel );
