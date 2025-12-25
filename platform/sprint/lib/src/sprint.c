ece6d22b636fcc7daa59f613dfd1ef9b702f427c
esmaniksic <esma.niksic@mikroe.com>
2025-12-25 16:14:44 +0100

    snprintf implementation started

--- platform/sprint/lib/src/sprint.c
+++ platform/sprint/lib/src/sprint.c
@@ -393,6 +393,403 @@ int sprintf_me( char *str, const char *format, ... ) {
     return count;
 }
 
+int snprintf_me( char *str, size_t size, const char *format, ... ) {
+    va_list args;
+    va_start( args, format );
+
+    char temp[32];
+    int count = 0;
+    size_t written = 0;
+
+    while ( *format ) {
+        if ( *format == '%' ) {
+            format++;
+
+            bool long_flag = false;
+            bool left_align = false;
+            bool zero_pad = false;
+            bool plus_sign = false;
+            bool space_sign = false;
+            bool alt_form = false;
+
+            int width = 0;
+            int precision = -1;
+
+            /* ---- FLAGS ---- */
+            bool parsing = true;
+            while ( parsing ) {
+                switch ( *format ) {
+                    case '-': left_align = true; format++; break;
+                    case '0': zero_pad = true; format++; break;
+                    case '+': plus_sign = true; format++; break;
+                    case ' ': space_sign = true; format++; break;
+                    case '#': alt_form = true; format++; break;
+                    default: parsing = false; break;
+                }
+            }
+
+            if ( left_align ) zero_pad = false;
+
+            /* ---- LENGTH ---- */
+            if ( *format == 'l' ) {
+                long_flag = true;
+                format++;
+            }
+            if ( *format == 'h' ) {
+                format++;
+            }
+
+            /* ---- WIDTH ---- */
+            while ( *format >= '0' && *format <= '9' ) {
+                width = width * 10 + ( *format - '0' );
+                format++;
+            }
+
+            /* ---- PRECISION ---- */
+            if ( *format == '.' ) {
+                format++;
+                precision = 0;
+                while ( *format >= '0' && *format <= '9' ) {
+                    precision = precision * 10 + ( *format - '0' );
+                    format++;
+                }
+            }
+
+            switch ( *format ) {
+            /* ---------- SIGNED INT ---------- */
+            case 'd': {
+                long value = long_flag ? va_arg( args, long ) : va_arg( args, int );
+                bool negative = value < 0;
+                if ( negative ) value = -value;
+
+                ltoa_me( value, temp );
+
+                int len = 0;
+                for ( char *p = temp; *p; p++ ) len++;
+
+                int sign_len = ( negative || plus_sign || space_sign ) ? 1 : 0;
+                int total = len + sign_len;
+                int pad = width > total ? width - total : 0;
+                char padc = zero_pad ? '0' : ' ';
+
+                if ( !left_align ) {
+                    if ( padc == ' ' ) {
+                        while ( pad-- ) {
+                            if ( written + 1 < size ) {
+                                *str++ = ' ';
+                            }
+                            written++;
+                            count++;
+                        }
+                    }
+                }
+
+                if ( negative ) {
+                    if ( written + 1 < size ) {
+                        *str++ = '-';
+                    }
+                    written++;
+                    count++;
+                }
+                else if ( plus_sign ) {
+                    if ( written + 1 < size ) {
+                        *str++ = '+';
+                    }
+                    written++;
+                    count++;
+                }
+                else if ( space_sign ) {
+                    if ( written + 1 < size ) {
+                        *str++ = ' ';
+                    }
+                    written++;
+                    count++;
+                }
+
+                if ( !left_align && padc == '0' ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = '0';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                for ( char *p = temp; *p; p++ ) {
+                    if ( written + 1 < size ) {
+                        *str++ = *p;
+                    }
+                    written++;
+                    count++;
+                }
+
+                if ( left_align ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = ' ';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+                break;
+            }
+
+            /* ---------- UNSIGNED / HEX / OCT ---------- */
+            case 'u':
+            case 'x':
+            case 'X':
+            case 'o': {
+                unsigned long value =
+                    long_flag ? va_arg( args, unsigned long ) : va_arg( args, unsigned int );
+
+                int base = ( *format == 'o' ) ? 8 : ( *format == 'u' ) ? 10 : 16;
+                bool upper = ( *format == 'X' );
+
+                int len = utoa_me( value, temp, base, upper );
+
+                int prefix_len = 0;
+                if ( alt_form && value ) {
+                    if ( *format == 'x' || *format == 'X' ) prefix_len = 2;
+                    if ( *format == 'o' ) prefix_len = 1;
+                }
+
+                int total = len + prefix_len;
+                int pad = width > total ? width - total : 0;
+                char padc = zero_pad ? '0' : ' ';
+
+                if ( !left_align && padc == ' ' ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = ' ';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                if ( prefix_len ) {
+                    if ( *format == 'o' ) {
+                        if ( written + 1 < size ) {
+                            *str++ = '0';
+                        }
+                        written++;
+                        count++;
+                    } else {
+                        if ( written + 1 < size ) {
+                            *str++ = '0';
+                        }
+                        written++;
+                        count++;
+
+                        if ( written + 1 < size ) {
+                            *str++ = upper ? 'X' : 'x';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                if ( !left_align && padc == '0' ) {
+                    while ( pad-- ) {
+                        if( written + 1 < size ) {
+                            *str++ = '0';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                for ( int i = 0; i < len; i++ ) {
+                    if ( written + 1 < size ) {
+                        *str++ = temp[i];
+                    }
+                    written++;
+                    count++;
+                }
+
+                if ( left_align ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = ' ';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+                break;
+            }
+
+            /* ---------- FLOAT ---------- */
+            case 'f': {
+                double value = va_arg( args, double );
+                if ( precision < 0 ) precision = 6;
+
+                bool negative = value < 0;
+                if ( negative ) value = -value;
+
+                unsigned long scale = 1;
+                for ( int i = 0; i < precision; i++ ) scale *= 10;
+
+                unsigned long long scaled = ( unsigned long long )( value * scale + 0.5 );
+                unsigned long ip = scaled / scale;
+                unsigned long fp = scaled % scale;
+
+                int ilen = utoa_me( ip, temp, 10, false );
+
+                char frac[32];
+                int flen = utoa_me( fp, frac, 10, false );
+
+                int sign_len = ( negative || plus_sign || space_sign ) ? 1 : 0;
+                int total = ilen + 1 + precision + sign_len;
+                int pad = width > total ? width - total : 0;
+                char padc = zero_pad ? '0' : ' ';
+
+                if ( !left_align && padc == ' ' ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = ' ';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                if ( negative ) {
+                    if( written + 1 < size ) {
+                        *str++ = '-';
+                    }
+                    written++;
+                    count++;
+                }
+                else if ( plus_sign ) {
+                    if( written + 1 < size ) {
+                        *str++ = '+';
+                    }
+                    written++;
+                    count++;
+                }
+                else if ( space_sign ) {
+                    if( written + 1 < size ) {
+                        *str++ = ' ';
+                    }
+                    written++;
+                    count++;
+                }
+
+                if ( !left_align && padc == '0' ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = '0';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+
+                for ( int i = 0; i < ilen; i++ ) {
+                    if ( written + 1 < size ) {
+                        *str++ = temp[i];
+                    }
+                    written++;
+                    count++;
+                }
+
+                if( written + 1 < size ) {
+                    *str++ = '.';
+                }
+                written++;
+                count++;
+
+                for ( int i = flen; i < precision; i++ ) {
+                    if ( written + 1 < size ) {
+                        *str++ = '0';
+                    }
+                    written++;
+                    count++;
+                }
+                for ( int i = 0; i < flen; i++ ) {
+                    if ( written + 1 < size ) {
+                        *str++ = frac[i];
+                    }
+                    written++;
+                    count++;
+                }
+
+                if ( left_align ) {
+                    while ( pad-- ) {
+                        if ( written + 1 < size ) {
+                            *str++ = ' ';
+                        }
+                        written++;
+                        count++;
+                    }
+                }
+                break;
+            }
+
+            case 'c': {
+                char ch = ( char )va_arg( args, int );
+                if ( written + 1 < size ) {
+                    *str++ = ch;
+                }
+                written++;
+                count++;
+                break;
+            }
+
+            case 's': {
+                char *s = va_arg( args, char* );
+                while ( *s ) {
+                    if ( written + 1 < size ) {
+                        *str++ = *s++;
+                    }
+                    written++;
+                    count++;
+                }
+                break;
+            }
+
+            case '%': {
+                if ( written + 1 < size ) {
+                    *str++ = '%';
+                }
+                written++;
+                count++;
+                break;
+            }
+
+            default:
+                if ( written + 2 < size ) {
+                    *str++ = '%';
+                    *str++ = *format;
+                }
+                written += 2;
+                count += 2;
+                break;
+            }
+        } else {
+            if ( written + 1 < size ) {
+                *str++ = *format;
+            }
+            written++;
+            count++;
+        }
+        format++;
+    }
+
+    if ( size > 0 ) {
+        if ( written >= size )
+            str[size - 1] = '\0';
+        else
+            *str = '\0';
+    }
+
+    va_end( args );
+    return count;
+}
+
 int sprintl_me( char *str, const char *format, ... ) {
     va_list args;
     va_start( args, format );
