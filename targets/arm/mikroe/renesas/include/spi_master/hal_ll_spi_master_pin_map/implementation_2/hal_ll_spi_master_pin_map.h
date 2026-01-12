f4a2a733d8eb2d0c86ae82c719eb74efa58c8412
mark0marko <simicmarko790@gmail.com>
2026-01-12 14:57:09 +0100

    initial complete sci spi pin map

--- targets/arm/mikroe/renesas/include/spi_master/hal_ll_spi_master_pin_map/implementation_2/hal_ll_spi_master_pin_map.h
+++ targets/arm/mikroe/renesas/include/spi_master/hal_ll_spi_master_pin_map/implementation_2/hal_ll_spi_master_pin_map.h
@@ -65,6 +65,30 @@ typedef struct {
 
 /*!< SPI SCK pins. */
 static const hal_ll_spi_master_pin_map_t hal_ll_spi_master_sck_map[] = {
+    #ifdef SPI0_SCK_P102_AF4
+    {GPIO_P102, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI0_SCK_P412_AF4
+    {GPIO_P412, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI1_SCK_P100_AF5
+    {GPIO_P100, HAL_LL_UART1_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_1 ), 5},
+    #endif
+    #ifdef SPI1_SCK_P112_AF5
+    {GPIO_P112, HAL_LL_UART1_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_1 ), 5},
+    #endif
+    #ifdef SPI2_SCK_P111_AF4
+    {GPIO_P111, HAL_LL_UART2_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_2 ), 4},
+    #endif
+    #ifdef SPI3_SCK_P410_AF5
+    {GPIO_P410, HAL_LL_UART3_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_3 ), 5},
+    #endif
+    #ifdef SPI4_SCK_P400_AF4
+    {GPIO_P400, HAL_LL_UART4_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_4 ), 4},
+    #endif
+    #ifdef SPI9_SCK_P111_AF5
+    {GPIO_P111, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
+    #endif
     #ifdef SPI9_SCK_P600_AF5
     {GPIO_P600, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
     #endif
@@ -74,6 +98,30 @@ static const hal_ll_spi_master_pin_map_t hal_ll_spi_master_sck_map[] = {
 
 /*!< SPI MISO pins. */
 static const hal_ll_spi_master_pin_map_t hal_ll_spi_master_miso_map[] = {
+    #ifdef SPI0_MISO_P100_AF4
+    {GPIO_P410, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI0_MISO_P410_AF4
+    {GPIO_P100, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI1_MISO_P212_AF5
+    {GPIO_P212, HAL_LL_UART1_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_1 ), 5},
+    #endif
+    #ifdef SPI2_MISO_P113_AF4
+    {GPIO_P113, HAL_LL_UART2_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_2 ), 4},
+    #endif
+    #ifdef SPI2_MISO_P301_AF4
+    {GPIO_P301, HAL_LL_UART2_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_2 ), 4},
+    #endif
+    #ifdef SPI3_MISO_P408_AF5
+    {GPIO_P408, HAL_LL_UART3_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_3 ), 5},
+    #endif
+    #ifdef SPI4_MISO_P206_AF4
+    {GPIO_P206, HAL_LL_UART4_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_4 ), 4},
+    #endif
+    #ifdef SPI9_MISO_P110_AF5
+    {GPIO_P110, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
+    #endif
     #ifdef SPI9_MISO_P601_AF5
     {GPIO_P601, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
     #endif
@@ -83,6 +131,33 @@ static const hal_ll_spi_master_pin_map_t hal_ll_spi_master_miso_map[] = {
 
 /*!< SPI MOSI pins. */
 static const hal_ll_spi_master_pin_map_t hal_ll_spi_master_mosi_map[] = {
+    #ifdef SPI0_MOSI_P101_AF4
+    {GPIO_P101, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI0_MOSI_P411_AF4
+    {GPIO_P411, HAL_LL_UART0_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_0 ), 4},
+    #endif
+    #ifdef SPI1_MOSI_P213_AF5
+    {GPIO_P213, HAL_LL_UART1_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_1 ), 5},
+    #endif
+    #ifdef SPI2_MOSI_P112_AF4
+    {GPIO_P112, HAL_LL_UART2_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_2 ), 4},
+    #endif
+    #ifdef SPI2_MOSI_P302_AF4
+    {GPIO_P302, HAL_LL_UART2_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_2 ), 4},
+    #endif
+    #ifdef SPI3_MOSI_P409_AF5
+    {GPIO_P409, HAL_LL_UART3_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_3 ), 5},
+    #endif
+    #ifdef SPI4_MOSI_P205_AF4
+    {GPIO_P205, HAL_LL_UART4_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_4 ), 4},
+    #endif
+    #ifdef SPI4_MOSI_P207_AF4
+    {GPIO_P207, HAL_LL_UART4_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_4 ), 4},
+    #endif
+    #ifdef SPI9_MOSI_P109_AF5
+    {GPIO_P109, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
+    #endif
     #ifdef SPI9_MOSI_P602_AF5
     {GPIO_P602, HAL_LL_UART9_BASE_ADDRESS, hal_ll_spi_master_module_num( SPI_MODULE_9 ), 5},
     #endif
