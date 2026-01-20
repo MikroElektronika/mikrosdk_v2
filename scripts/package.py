ffed6ea1f2fe61e3bd447c4f69b8880da0d8538f
esmaniksic <esma.niksic@mikroe.com>
2026-01-20 12:10:53 +0100

    Update in templates

--- scripts/package.py
+++ scripts/package.py
@@ -469,7 +469,7 @@ def package_templates_files(templates_root_path, path_list, necto_version, asset
         archive_path = os.path.join(templates_root_path, archive_folder_name)
         if 'project_templates' in folder and 'lvgl' in folder:
             for lvgl_folder in os.listdir(os.path.join(templates_root_folder, folder)):
-                archive_folder_name = f'templates_{necto_version}_2_lvgl{lvgl_folder[-1]}_designer.7z'
+                archive_folder_name = f'templates_{necto_version}_2_lvgl{lvgl_folder[5:]}_designer.7z'
                 archive_path = os.path.join(templates_root_path, archive_folder_name)
                 create_custom_archive(
                     os.path.join(templates_root_folder, folder, lvgl_folder),
