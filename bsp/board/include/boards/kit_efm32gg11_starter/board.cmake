6722754365dd17e3d402645b9c3efdb242904697
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:03:01 +0200

    Added release hw to new-feature/boards/1412

---
+++ bsp/board/include/boards/kit_efm32gg11_starter/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFM32GG11_STARTER")
+    set(BOARD_PATH "include/boards/kit_efm32gg11_starter")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
