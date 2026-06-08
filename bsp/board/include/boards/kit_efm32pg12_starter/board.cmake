cf47f3c32e8a7c89a3b47bf736d5c9e9ddead4ad
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:02:58 +0200

    Added release hw to new-feature/boards/1112

---
+++ bsp/board/include/boards/kit_efm32pg12_starter/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFM32PG12_STARTER")
+    set(BOARD_PATH "include/boards/kit_efm32pg12_starter")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
