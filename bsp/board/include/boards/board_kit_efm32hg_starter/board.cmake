687c7b10f7473b5c8e737f6b742e0581b45afe6d
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:25:08 +0200

    Added release hw to new-feature/boards/0912

--- bsp/board/include/boards/board_kit_efm32hg_starter/board.cmake
+++ bsp/board/include/boards/board_kit_efm32hg_starter/board.cmake
@@ -1,5 +1,5 @@
 if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFM32HG_STARTER")
-    set(BOARD_PATH "include/boards/kit_efm32hg_starter")
+    set(BOARD_PATH "include/boards/board_kit_efm32hg_starter")
     set(MCU_CARD FALSE)
     set(SHIELD FALSE)
     set(DIP_SOCKET FALSE)
