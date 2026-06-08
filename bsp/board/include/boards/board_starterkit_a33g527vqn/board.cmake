dfc48cf988cc28b9e8e9dd289ac2fe3b38277eb9
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:25:50 +0200

    Added release hw to new-feature/boards/2812

--- bsp/board/include/boards/board_starterkit_a33g527vqn/board.cmake
+++ bsp/board/include/boards/board_starterkit_a33g527vqn/board.cmake
@@ -1,5 +1,5 @@
 if(${_MSDK_BOARD_NAME_} STREQUAL "STARTERKIT_A33G527VQN")
-    set(BOARD_PATH "include/boards/starterkit_a33g527vqn")
+    set(BOARD_PATH "include/boards/board_starterkit_a33g527vqn")
     set(MCU_CARD FALSE)
     set(SHIELD FALSE)
     set(DIP_SOCKET FALSE)
