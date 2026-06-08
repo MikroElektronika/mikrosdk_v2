e58ff146d96c4478ef855ff456d9787a2c68d35d
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:04:32 +0200

    Added release hw to new-feature/boards/2201

---
+++ bsp/board/include/boards/starterkit_a31g346rln/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "STARTERKIT_A31G346RLN")
+    set(BOARD_PATH "include/boards/starterkit_a31g346rln")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
