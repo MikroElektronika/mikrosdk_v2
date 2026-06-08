d76378136ecd10835431f2776607334fe6011409
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:02:26 +0200

    Added release hw to new-feature/boards/2711

---
+++ bsp/board/include/boards/kit_efr32xg28_explorer/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFR32XG28_EXPLORER")
+    set(BOARD_PATH "include/boards/kit_efr32xg28_explorer")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
