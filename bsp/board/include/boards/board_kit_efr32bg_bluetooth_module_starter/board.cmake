2d024a0bcfaf7bde847fa71af0b3bdd7e52e3275
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:25:34 +0200

    Added release hw to new-feature/boards/2112

--- bsp/board/include/boards/board_kit_efr32bg_bluetooth_module_starter/board.cmake
+++ bsp/board/include/boards/board_kit_efr32bg_bluetooth_module_starter/board.cmake
@@ -1,5 +1,5 @@
 if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFR32BG_BLUETOOTH_MODULE_STARTER")
-    set(BOARD_PATH "include/boards/kit_efr32bg_bluetooth_module_starter")
+    set(BOARD_PATH "include/boards/board_kit_efr32bg_bluetooth_module_starter")
     set(MCU_CARD FALSE)
     set(SHIELD FALSE)
     set(DIP_SOCKET FALSE)
