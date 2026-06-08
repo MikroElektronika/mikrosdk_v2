1e37240bd5197253add3913c4b6cbeecf86e0783
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-08 14:02:11 +0200

    Added release hw to new-feature/boards/2011

---
+++ bsp/board/include/boards/kit_efm32pg26_explorer/board.cmake
@@ -0,0 +1,6 @@
+if(${_MSDK_BOARD_NAME_} STREQUAL "KIT_EFM32PG26_EXPLORER")
+    set(BOARD_PATH "include/boards/kit_efm32pg26_explorer")
+    set(MCU_CARD FALSE)
+    set(SHIELD FALSE)
+    set(DIP_SOCKET FALSE)
+endif()
