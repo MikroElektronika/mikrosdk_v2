9fcd0d0ccc74be0c1183c6d797d3f743b8e98882
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-06-18 12:11:50 +0200

    Fixed RA6T2 mcu definition files

--- targets/arm/mikroe/common/include/mcu_definitions/RENESAS/R7FA6T2AD3CFL/mcu_definitions.h
+++ targets/arm/mikroe/common/include/mcu_definitions/RENESAS/R7FA6T2AD3CFL/mcu_definitions.h
@@ -93,7 +93,40 @@
 //EOF GPIO
 
 //ADC
-#define ADC_MODULE_COUNT 0
+#ifdef __PA00_CN
+#define ADC0_PA00_AN000
+#endif
+#ifdef __PA01_CN
+#define ADC0_PA01_AN001
+#endif
+#ifdef __PA02_CN
+#define ADC0_PA02_AN002
+#endif
+#ifdef __PA03_CN
+#define ADC0_PA03_AN003
+#endif
+#ifdef __PA04_CN
+#define ADC0_PA04_AN004
+#endif
+#ifdef __PA05_CN
+#define ADC0_PA05_AN005
+#endif
+#ifdef __PA06_CN
+#define ADC0_PA06_AN006
+#endif
+#ifdef __PA07_CN
+#define ADC0_PA07_AN007
+#endif
+#ifdef __PB00_CN
+#define ADC0_PB00_AN008
+#endif
+#ifdef __PB01_CN
+#define ADC0_PB01_AN009
+#endif
+
+#define ADC_MODULE_0 1
+
+#define ADC_MODULE_COUNT 1
 //EOF ADC
 
 //SCI
@@ -222,7 +255,7 @@
 
 //I2C
 #ifdef __PA08_CN
-#define I2C0_SCL0_D_PA08_AF7
+#define I2C0_SCL_PA08_AF7
 #endif
 #ifdef __PB06_CN
 #define I2C0_SCL_PB06_AF7
